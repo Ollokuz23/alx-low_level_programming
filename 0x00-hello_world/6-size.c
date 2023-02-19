a3b5b927f97936185455a99830cc3f736a624d81
Ollokuz23 <willilamclintonotieno12@gmail.com>
2023-02-19 00:28:48 -0800

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - prints the sizes of variables types
+ *
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	char c;
+	int i;
+	long int li;
+	long long int lli;
+	float f;
+	printf("size of a char: %lu\n", (long unsigned)(sizeof(c)));
+	printf("size of an int: %lu\n", (long unsigned)(sizeof(i)));
+	printf("size of a long int: %lu\n", (long unsigned)(sizeof(li)));
+	printf("size of a long long int: %lu\n", (long unsigned)(sizeof(lli)));
+	printf("size of a float: %lu\n", (long unsigned)(sizeof(f)));
+}
