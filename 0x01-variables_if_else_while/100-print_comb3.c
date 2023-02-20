71a891a2b31714eb26defbcf15ffe028bc505fc0
Ollokuz23 <willilamclintonotieno12@gmail.com>
2023-02-20 14:29:45 -0800

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -7,6 +7,8 @@
 int main(void)
 {
 	int num1, num2;
+
+
 	for (num1 = 0; num1 < 9; num1++)
 	{
 		for (num2 = num1 + 1; num2 < 10; num2++)
@@ -15,8 +17,8 @@ int main(void)
 			putchar((num2 % 10) + '0');
 			if (num1 == 8 && num2 == 9)
 				continue;
-			putchar(' , ');
-			putchar('  ');
+			putchar(',');
+			putchar(' ');
 		}
 	}
 	putchar('\n');
