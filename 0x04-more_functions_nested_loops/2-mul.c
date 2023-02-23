61f8555ab61034265938790604d58a084ccbe0ab
Ollokuz23 <willilamclintonotieno12@gmail.com>
2023-02-23 02:29:51 -0800

    2. Collaboration is multiplication

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * mul - multiplies two integers
+ * @a: one of the integer involved
+ * @b: the other integer involved
+ *
+ * Return: the multiplication result
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
--- 0x04-more_functions_nested_loops/README.md
+++ 0x04-more_functions_nested_loops/README.md
@@ -1,2 +1,3 @@
 0. isupper : Write a function that checks for uppercase character.
 1. isdigit
+Writes a function that multiplies two integers
