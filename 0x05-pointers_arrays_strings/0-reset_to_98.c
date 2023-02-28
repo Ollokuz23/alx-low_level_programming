9935f7bd3d6d24bdcc2d8d58d37fc523f00d2ed1
Ollokuz23 <willilamclintonotieno12@gmail.com>
2023-02-28 13:54:44 -0800

    Writes a function that takes a pointer to an int as parameter

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - updates the value to a pointer
+ * @n : integer we're resetting to
+ *
+ * Return: the integer values
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
