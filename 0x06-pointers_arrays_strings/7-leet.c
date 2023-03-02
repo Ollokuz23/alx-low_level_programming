c708e94c337d129b1e27dbd56e92473b869a4d14
Ollokuz23 <willilamclintonotieno12@gmail.com>
2023-03-02 03:13:47 -0800

    Encodes a string

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * leet - encodes a string
+ * @str: the string to be encoded
+ *
+ * Return: encoded string
+ */
+char *leet(char *str)
+{
+	int in1 = 0, in2;
+	char leet[8] = {'O', 'L', '?', 'E', 'A', '?', '?', 'T'};
+
+	while (str[in1])
+	{
+		for (in2 = 0; in2 <= 7; in2++)
+		{
+			if
+				(str[in1] == leet[in2] ||
+				 str[in1] - 32 == leet[in2])
+					str[in1] = in2 + '0';
+		}
+		in1++;
+	}
+	return (str);
+}
+
