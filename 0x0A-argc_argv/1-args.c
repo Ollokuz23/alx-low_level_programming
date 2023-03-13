1023a5118a007f5f651e8ebdb6283cbb956695f2
Ollokuz23 <willilamclintonotieno12@gmail.com>
2023-03-13 11:26:36 -0700

    1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - check code
+ * @argc: counts the arguments no.
+ * @argv: array to a pointer
+ *
+ * Return: 0 (Success)
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc - 1);
+	(void)argv;
+	return (0);
+}
