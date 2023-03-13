10f779592ab6f7b51923024d7ff1b2af585aad80
Ollokuz23 <willilamclintonotieno12@gmail.com>
2023-03-13 10:31:20 -0700

    0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,6 +1,14 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+/**
+ * main - check code
+ * @argc: counts the arguments
+ * @argv: array to a pointer
+ *
+ * Return: 0 (Success)
+ */
+
 int main(int argc, char *argv[])
 {
 	printf("%s\n", argv[0]);
