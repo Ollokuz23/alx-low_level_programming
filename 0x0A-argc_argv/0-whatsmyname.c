c7508ca61aa9c1691543377a3ae2606e620b22a4
Ollokuz23 <willilamclintonotieno12@gmail.com>
2023-03-13 10:17:53 -0700

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,9 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	(void)argc;
+	return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,1 @@
+I learnt about argc & argv in c
